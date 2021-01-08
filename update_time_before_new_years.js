ab5395c118cfc2a36ca4427f3cf8abc89cb5054d
Melvynx <melvyn.malherbe@qoqa.com>
2021-01-08 13:21:31 +0100

    add loggs

--- update_time_before_new_years.js
+++ update_time_before_new_years.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 var exec = require('child_process').exec;
 
+console.log('START TEST');
 exec('ls -la', function (err, stdout, stderr) {
   console.log(stdout);
 });
@@ -8,6 +9,7 @@ exec('ls -la', function (err, stdout, stderr) {
 const msInOneDay = 1000 * 60 * 60 * 24;
 
 function readREADME() {
+  console.log('START READING');
   return new Promise((resolve, reject) => {
     fs.readFile('./README.md', 'utf8', function (err, data) {
       if (err) {
