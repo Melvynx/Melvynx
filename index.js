ea9730693fb02e7447e11781ba8bdf460af55ca7
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2022-11-02 22:58:27 +0100

    remove my age

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function generateNewREADME() {
 
   function updateIdentifier(identifier, replaceText) {
     const identifierIndex = findIdentifierIndex(readmeRow, identifier);
+    if (!readmeRow[identifierIndex]) return;
     readmeRow[identifierIndex] = readmeRow[identifierIndex].replace(
       `<#${identifier}>`,
       replaceText
@@ -19,7 +20,6 @@ function generateNewREADME() {
   const identifierToUpdate = {
     // * DBNW = Day Before New Year
     day_before_new_years: getDBNWSentence(),
-    age_and_birthday: getAgeAndBirthdaySentence(),
     myself: getMySelf(),
     today_date: getTodayDate(),
     gabot_signing: getGabotSigning(),
@@ -58,35 +58,6 @@ function getMySelf() {
       ? 'penguin 🐧'
       : 'bear 🐻'
     : 'penguin bear 🐧🐻';
-}
-
-function getAgeAndBirthdaySentence() {
-  const birthday = new Date('2002-05-06T00:00:00.000Z');
-
-  const diffBirthdayToToday = today - birthday;
-
-  // to get my current ages
-  const age = new Date(diffBirthdayToToday).getFullYear() - 1970;
-
-  if (
-    birthday.getDate() === today.getDate() &&
-    birthday.getMonth() === today.getMonth()
-  )
-    return `Today is my birthday🎉! I am ${age} years old.`;
-
-  const birthdatetoday = new Date(birthday.setYear(today.getFullYear()));
-  const isBirthdayRaised = today - birthdatetoday > 0;
-
-  const nextBirthdayYear = birthday.getFullYear() + (isBirthdayRaised ? 1 : 0);
-  const nextBirthdayDate = new Date(birthday.getTime());
-  nextBirthdayDate.setYear(nextBirthdayYear);
-
-  const timeUntilBirthday = nextBirthdayDate - today;
-  const dayUntilBirthday = Math.round(timeUntilBirthday / msInOneDay);
-
-  return `I am ${age} years old... But I will be ${
-    age + 1
-  } in ${dayUntilBirthday} days 🎉`;
 }
 
 function getDBNWSentence() {
