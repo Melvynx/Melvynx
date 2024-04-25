301092bb005a381af53e4ac6e1cb0b893915aa0b
Melvyn <56388157+Melvynx@users.noreply.github.com>
2024-04-25 09:59:02 +0800

    Update readme.js

--- readme.js
+++ readme.js
@@ -6,36 +6,49 @@ And... I create online courses in french to help developers to master modern web
 
 ### Contact me
 
-* [Twitter](https://twitter.com/melvynxdev)
-* [Youtube](https://www.youtube.com/channel/UC5HDIVwuqoIuKKw-WbQ4CvA)
+* [Twitter](https://mlv.sh/twitter)
+* [Youtube](https://mlv.sh/youtube)
 * [Mail](mailto:contact@melvynx.com)
-* [Linkedin](https://www.linkedin.com/in/melvyn-malherbe/)
+* [Linkedin](https://mlv.sh/linkedin)
+* [Thread](https://mlv.sh/thread)
 
 ### My projects
 
-* [Melvynx](https://melvynx.com)
+* [Melvynx](https://mlv.sh)
 
 My personal website, where you can find my projects, my blog and my resume.
 
-* [Codelynx](https://codelynx.dev)
+* [Codelynx](https://mlv.sh/codelynx)
 
 My blog and my newsletter website.
 
-* [Codeline](https://app.codelynx.dev)
+* [Codeline](https://mlv.sh/codeline)
 
 My online courses plateforme. I created BeginReact, NextReact and BeginJavaScript.
 
-* [Chat2Code](https://chat2code.dev)
+* [Chat2Code](https://mlv.sh/chat2code)
 
 A ChatGPT like application that create and generate component directly in the browser. Stop wasting time copy pasting code from ChatGPT to your editor, Chat2Code bring editor in the chat.
 
-* [QuizUp](https://quizup.app)
+* [QuizUp](https://mlv.sh/quizup)
 
 Create a quiz with multiple choice and open questions with input. The next generation of quiz.
 
-* [BulkCorrector](https://bulkcorrector.com)
+* [BulkCorrector](https://mlv.sh/bulkcorrector)
 
 Correct the grammar of an entire book in 5 minutes.
+
+* [LinkedLeads](https://mlv.sh/linkedleads)
+
+Accept all your invitations on LinkedIn.
+
+* [PowerPost](https://mlv.sh/powerpost)
+
+Start reading posts FASTER.
+
+* [PowerNote](https://mlv.sh/powernote)
+
+Create a daily note without any friction.
 
 ### Visitor count
 
